b0d241b2fe922222116105f99f8757b7ba2474c2
soojiyou <sooji.you@wsu.edu>
2023-04-20 18:50:49 -0700

    models and admin app

--- config/settings.py
+++ config/settings.py
@@ -30,11 +30,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-CUSTOM_APPS = ["common.apps.CommonConfig",
-               "users.apps.UsersConfig",
-               "rooms.apps.RoomsConfig",
-               "experiences.apps.ExperiencesConfig",
-               "categories.apps.CategoriesConfig"]
+CUSTOM_APPS = [
+    "common.apps.CommonConfig",
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+    "experiences.apps.ExperiencesConfig",
+    "categories.apps.CategoriesConfig",
+    "reviews.apps.ReviewsConfig",
+    "wishlists.apps.WishlistsConfig",
+    "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
+    "direct_messages.apps.DirectMessagesConfig"
+]
 
 SYSTEM_APPS = [
     'django.contrib.admin',
