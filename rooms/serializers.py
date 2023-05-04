ca1781a1fb0d27162e296ae05c7f955ac7851bb4
soojiyou <sooji.you@wsu.edu>
2023-05-03 21:47:06 -0700

    roomdetail page initial commit

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -61,7 +61,9 @@ class RoomDetailSerializer(serializers.ModelSerializer):
 
     def get_is_liked(self, room):
         request = self.context["request"]
-        return Wishlist.objects.filter(user=request.user, rooms__id=room.pk).exists()
+        if request.user.is_authenticated:
+            return Wishlist.objects.filter(user=request.user, rooms__id=room.pk).exists()
+        return False
 
 
 class UserRoomSerializer(serializers.ModelSerializer):
